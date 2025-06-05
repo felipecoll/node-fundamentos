877e286354639b3cc7696c07cc044cea2ef07234
Felipe Coll <leonfelipecoll@gmail.com>
2025-06-05 11:02:06 -0300

    Ejemplos varios de modulos, exportaciones, etc

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const math = require('./math.js');
+
+console.log(`Sumar:${math.add(5,3)}`); // Output: 8
+console.log(`Restar:${math.subtract(5, 3)}`); // Output: 2
---
+++ src/main.mjs
@@ -0,0 +1,4 @@
+import { multiply, divide } from "./math.mjs";
+
+console.log(`Multiplicar: ${multiply(5, 3)}`); // Output: 15
+console.log(`Dividir: ${divide(5, 3)}`); // Output: 1.6666666666666667
