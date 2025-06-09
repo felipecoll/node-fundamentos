634ad594be2e29c0978231015807862e7f3ecc4b
Felipe Coll <leonfelipecoll@gmail.com>
2025-06-09 14:58:23 -0300

    Ejemplos con isnumber

--- npm_publish/main.js
+++ npm_publish/main.js
@@ -1,1 +1,8 @@
 console.log('Publishing package to npm...');
+
+var isNumber = require('is-number');
+
+isNumber(123); // true
+
+console.log(isNumber('123')); // true
+console.log(isNumber('abc')); // false
